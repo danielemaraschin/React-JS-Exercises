661a80e31a1767bea4899aba0cd616cf474acf1d
danielemaraschin <danikmaraschin@gmail.com>
2021-05-19 14:17:22 +0200

    Learning about destructuring

--- destructuring.js
+++ destructuring.js
@@ -9,3 +9,10 @@ console.log(numeros)
 const numeros1 = [...numerosPares, ...numerosImpares]
 
 console.log(numeros1)
+
+const num1 = 1
+const num2 = 2
+console.log(num1, num2)
+
+const [nume1, nume2] = [1,2]
+console.log(nume1, nume2)
